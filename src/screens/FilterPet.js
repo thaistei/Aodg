73df35c25af9a3ebcad5f828a7f069bf6758d00e
thaisteixeira <171821+thaistei@users.noreply.github.com>
2024-07-31 10:54:39 -0300

    linda

---
+++ src/screens/FilterPet.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import { View, Text, Button, Picker, Slider, StyleSheet, TouchableOpacity } from 'react-native';
+import RadioButton from 'react-native-radio-button';
+
+
+export default function PetFilter() {
+  const [animalType, setAnimalType] = useState(null);
+  const [gender, setGender] = useState(null);
+  const [breed, setBreed] = useState('');
+  const [age, setAge] = useState(0);
+  const [size, setSize] = useState(null);
+
+  const handleSave = () => {
+  
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha o tipo de animal que procura*</Text>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => setAnimalType('cachorro')} style={[styles.button, animalType === 'cachorro' && styles.active]}>
+          <Text>Cachorro</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setAnimalType('gato')} style={[styles.button, animalType === 'gato' && styles.active]}>
+          <Text>Gato</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <Text style={styles.title}>Sexo do pet (opcional)</Text>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => setGender('Macho')} style={[styles.button, gender === 'Macho' && styles.active]}>
+          <Text>Macho</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setGender('Femea')} style={[styles.button, gender === 'Femea' && styles.active]}>
+          <Text>Fêmea</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.title}>Raças disponíveis</Text>
+      <Picker
+        selectedValue={breed}
+        onValueChange={(itemValue) => setBreed(itemValue)}
+        style={styles.picker}
+      >
+       
+        <Picker.Item Text="Digite uma raça" value="" />
+      </Picker>
+
+      <Text style={styles.title}>Idade (de 0 a 20)*</Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={0}
+        maximumValue={20}
+        step={1}
+        value={age}
+        onValueChange={(value) => setAge(value)}
+      />
+      <Text>Idade: {age} anos</Text>
+
+      <Text style={styles.title}>Porte*</Text>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => setSize('P')} style={[styles.sizeButton, size === 'P' && styles.active]}>
+          <Text>P</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSize('M')} style={[styles.sizeButton, size === 'M' && styles.active]}>
+          <Text>M</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSize('G')} style={[styles.sizeButton, size === 'G' && styles.active]}>
+          <Text>G</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Button title="Salvar" onPress={handleSave} style={styles.saveButton} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  active: {
+    backgroundColor: '#add8e6',
+  },
+  picker: {
+    marginVertical: 10,
+  },
+  slider: {
+    width: '100%',
+    height: 40,
+  },
+  sizeButton: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  saveButton: {
+    marginTop: 20,
+    backgroundColor: '#00f',
+    color: '#fff',
+  },
+});
---
+++ src/screens/RegisterUser.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const RegistrationScreen = () => {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.logoContainer}>
+        <Image source={require('./assets/LogoAdog.png')} style={styles.logo} />
+      </View>
+
+      
+      <Text style={styles.title}>Como deseja se cadastrar?</Text>
+
+      
+      <TouchableOpacity style={styles.button}>
+        <View style={styles.buttonContent}>
+          <Image source={require('./assets/iconedepata-removebg-preview.png')} style={styles.icon} />
+          <Text style={styles.buttonText}>Pretendente a Adoção</Text>
+        </View>
+      </TouchableOpacity>
+
+    
+      <TouchableOpacity style={styles.button}>
+        <View style={styles.buttonContent}>
+          <Image source={require('./assets/icone_de_dinheiro-removebg-preview.png')} style={styles.icon} />
+          <Text style={styles.buttonText}>Parceiro</Text>
+        </View>
+      </TouchableOpacity>
+
+      
+     
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    backgroundColor: '#00008B', // Azul escuro
+    width: '100%',
+    paddingVertical: 20,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  logo: {
+    width: 150,
+    height: 50,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#00008B', // Azul escuro
+    borderRadius: 30,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  }
+});
+
+export default RegistrationScreen;
